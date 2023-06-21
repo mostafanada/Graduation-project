bfa61f3c953d72b3abbdbed6fa9b9b4a415f5f00
Shrouk Salah <Shroukgalbat0@gmail.com>
2023-06-21 04:51:46 +0300

    Add labels filter & categorize them

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+List<String> dangerList = [];
+List<String> normalDangerList = [];
+
+Future<void> main() async {
   runApp(MyApp());
+  String assetPathDanger = "assets/RedList.txt";
+  String fileDangerContent = await rootBundle.loadString(assetPathDanger);
+  dangerList = fileDangerContent.split('\n');
+  for (int i = 0; i < dangerList.length; i++) {
+    dangerList[i] = dangerList[i].trim();
+  }
+  dangerList.sort();
+
+  String assetPathNormalDanger = "assets/NormalList.txt";
+  String fileNormalDangerContent =
+      await rootBundle.loadString(assetPathNormalDanger);
+  normalDangerList = fileNormalDangerContent.split('\n');
+  for (int i = 0; i < normalDangerList.length; i++) {
+    normalDangerList[i] = normalDangerList[i].trim();
+  }
+  normalDangerList.sort();
 }
 
 class MyApp extends StatefulWidget {
@@ -11,8 +31,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String finalOutput = '';
-
+  String dangerOutput = '';
+  String notDangerOutput = '';
+  String normalDangerOutput = '';
   @override
   void initState() {
     super.initState();
@@ -21,7 +42,29 @@ class _MyAppState extends State<MyApp> {
     // Set up a listener to receive events from the EventChannel
     channel.receiveBroadcastStream().listen((event) {
       setState(() {
-        finalOutput = event;
+        String splitedString = event;
+        List<String> curLine = splitedString.split('\n');
+        String danger = '';
+        String nonDanger = '';
+        String normalDanger = '';
+        for (int i = 0; i < curLine.length; i++) {
+          List<String> words = curLine[i].split(':');
+          if (binarySearch(dangerList, words[0]) != -1) {
+            danger = words[0];
+            danger += '\n';
+          } else if (binarySearch(normalDangerList, words[0]) != -1) {
+            normalDanger = words[0];
+            normalDanger += '\n';
+          } else {
+            nonDanger += words[0];
+            nonDanger += '\n';
+          }
+        }
+        dangerOutput += danger;
+        normalDangerOutput += normalDanger;
+        if (nonDanger != 'Could not classify') {
+          notDangerOutput = nonDanger;
+        }
       });
     });
   }
@@ -34,10 +77,21 @@ class _MyAppState extends State<MyApp> {
           title: Text('Final Output'),
         ),
         body: Center(
-          child: Text(finalOutput),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Danger Output:'),
+              Text(dangerOutput),
+              SizedBox(height: 20),
+              Text('Normal Danger Output:'),
+              Text(normalDangerOutput),
+              SizedBox(height: 20),
+              Text('Non-Danger Output:'),
+              Text(notDangerOutput),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-p
