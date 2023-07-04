c323d39df6292cc15cc1e8d2bffa772f16f76c86
mostafanada <mostafanada85110@yahoo.com>
2023-07-05 01:31:00 +0300

    edit the output

--- lib/Screens/OutputScreen/Serves/all_function.dart
+++ lib/Screens/OutputScreen/Serves/all_function.dart
@@ -7,28 +7,26 @@ class Pair<T1, T2> {
   Pair(this.first, this.second);
 }
 
-class attrbuties {
-  String assetPathDanger = "assets/RedList.txt";
-  String assetPathNormalDanger = "assets/NormalList.txt";
-  List<String> dangerLabelsList = [];
-  List<String> normalDangerList = [];
+String assetPathDanger = "assets/RedList.txt";
+String assetPathNormalDanger = "assets/NormalList.txt";
+List<String> dangerLabelsList = [];
+List<String> normalDangerList = [];
 
-  void dangerListFun() async {
-    String fileDangerContent = await rootBundle.loadString(assetPathDanger);
-    dangerLabelsList = fileDangerContent.split('\n');
-    for (int i = 0; i < dangerLabelsList.length; i++) {
-      dangerLabelsList[i] = dangerLabelsList[i].trim();
-    }
-    dangerLabelsList.sort();
+void dangerListFun() async {
+  String fileDangerContent = await rootBundle.loadString(assetPathDanger);
+  dangerLabelsList = fileDangerContent.split('\n');
+  for (int i = 0; i < dangerLabelsList.length; i++) {
+    dangerLabelsList[i] = dangerLabelsList[i].trim();
   }
+  dangerLabelsList.sort();
+}
 
-  void normalListFun() async {
-    String fileNormalDangerContent =
-        await rootBundle.loadString(assetPathNormalDanger);
-    normalDangerList = fileNormalDangerContent.split('\n');
-    for (int i = 0; i < normalDangerList.length; i++) {
-      normalDangerList[i] = normalDangerList[i].trim();
-    }
-    normalDangerList.sort();
+void normalListFun() async {
+  String fileNormalDangerContent =
+      await rootBundle.loadString(assetPathNormalDanger);
+  normalDangerList = fileNormalDangerContent.split('\n');
+  for (int i = 0; i < normalDangerList.length; i++) {
+    normalDangerList[i] = normalDangerList[i].trim();
   }
+  normalDangerList.sort();
 }
