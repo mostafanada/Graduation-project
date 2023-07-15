6c9d992e0b0d93b95be51991a3a6c6a5b6cb40e8
Hany Al-Kelany <84659136+Hany-kelany@users.noreply.github.com>
2023-07-16 01:22:15 +0300

    Update sendEmailScreen.dart

--- lib/Screens/contactus/sendemail/sendEmailScreen.dart
+++ lib/Screens/contactus/sendemail/sendEmailScreen.dart
@@ -7,6 +7,7 @@ class sendEmailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         leading: GestureDetector(
             onTap: () {
               Navigator.pushReplacement(
